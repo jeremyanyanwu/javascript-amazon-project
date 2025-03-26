2ee378511490c238b8a282f14344878c36ad51fe
jeremyanyanwu <janyanwu@ggc.edu>
2025-03-26 09:15:55 -0400

    Covert loadproducts from xhr to fetch request

--- javascript-amazon-project-main/tests-jasmine/checkout/orderSummaryTest.js
+++ javascript-amazon-project-main/tests-jasmine/checkout/orderSummaryTest.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 
 import {loadFromStorage, cart } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProducts, loadProductsFetch } from "../../data/products.js";
 
 
 describe('test suite: renderOrderSummary',() => {
@@ -11,7 +11,7 @@ describe('test suite: renderOrderSummary',() => {
 
 
   beforeAll((done) => {
-    loadProducts(() => {
+    loadProductsFetch().then(() => {
       done();
     });
   });
